b8e439dfe1dcb872d7c17d3cc6d350f68808a0a7
Eric Nguyen <ericnguyen13@hotmail.com>
2014-12-23 09:08:43 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    ValidatorFactory: require('./lib/validators/validatorFactory'),
+    NodeFactory: require('./lib/factories/nodeFactory'),
+    Constants: require('./lib/constants/constants')
+};
