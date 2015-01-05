0753dcf7aeffaa8316d69546583baa0316b72a16
Eric Nguyen <ericnguyen13@hotmail.com>
2015-01-04 22:56:03 -0700

    Expose the TypeChecker to the client.

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 module.exports = {
     ValidatorFactory: require('./lib/validators/validatorFactory'),
     NodeFactory: require('./lib/factories/nodeFactory'),
-    Constants: require('./lib/constants/constants')
+    Constants: require('./lib/constants/constants'),
+    TypeChecker: require('./lib/TypeChecker')
 };
